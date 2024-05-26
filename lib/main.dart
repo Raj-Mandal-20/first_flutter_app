c29fa0461af9e21bffd523d882f6688f715c8ac0
Raj-Mandal-20 <usingrajcseinfo@gmail.com>
2024-05-26 15:03:30 +0530

    adding image

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,32 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-void main() => runApp(MaterialApp(
-  home: Scaffold(
-    appBar: AppBar(
-      title: const Text('First APP 😍'),
-      centerTitle: true,
-      backgroundColor: Colors.blueGrey,
-
-    ),
-    body: const Center(
-      child : Text(
-        "This is a very Beautiful App 😽",
-        style: TextStyle(
-          fontWeight: FontWeight.bold,
-          fontSize: 20,
-          fontFamily: 'IndieFlower',
-        ),
-      ),
-
-    ),
-    floatingActionButton : FloatingActionButton(
-      onPressed: () {  },
-      child : Text('Click'),
-      backgroundColor: Colors.amber[600],
-    ),
-  )
+void main() => runApp(const MaterialApp(
+  home:  Home()
 ));
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('First APP 😍'),
+        centerTitle: true,
+        backgroundColor: Colors.blueGrey,
+
+      ),
+      body: const Center(
+        child : Image(
+          image : NetworkImage('https://images.unsplash.com/photo-1716553313539-92c68fde5bca?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')
+        )
+
+      ),
+      floatingActionButton : FloatingActionButton(
+        onPressed: () {  },
+        child : Text('Click Me'),
+        backgroundColor: Colors.amber[600]
+      ),
+    );
+  }
+}
