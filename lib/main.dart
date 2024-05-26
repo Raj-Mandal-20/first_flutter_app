dd1d77d1ae4b4d973f92a1f0ed76d9c4fc5efcd0
Raj-Mandal-20 <usingrajcseinfo@gmail.com>
2024-05-26 15:13:47 +0530

    learning basic containers

--- lib/main.dart
+++ lib/main.dart
@@ -16,17 +16,15 @@ class Home extends StatelessWidget {
         backgroundColor: Colors.blueGrey,
 
       ),
-      body: const Center(
-        child : Image(
-          image : NetworkImage('https://images.unsplash.com/photo-1716553313539-92c68fde5bca?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')
-        )
-
-      ),
-      floatingActionButton : FloatingActionButton(
-        onPressed: () {  },
-        child : Text('Click Me'),
-        backgroundColor: Colors.amber[600]
-      ),
+      body: Center(
+        child: Container(
+          width: 200,
+          height: 200,
+          color: Colors.lightBlue,
+          child : Center(child: Text('Container'))
+              
+        ),
+      )
     );
   }
 }
